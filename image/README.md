53dae06c76af81ebbdbd50dd22bfe3b7f503af67
Pitikron <130220609+PitikronTutourial@users.noreply.github.com>
2024-11-28 21:13:52 +0700

    Use Eclipe to Run

---
+++ image/README.md
@@ -0,0 +1,2 @@
+# Gameknightkinddom
+Use Eclipse IDE for Java Developers To run or Other Program
---
+++ src/main/Mainclass.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		new Game();
+
+	}
+
+}
